8add2a4fbe912e0534bb3108a851365ebd085c2b
Berfin <101349315+BerfinUn@users.noreply.github.com>
2024-10-05 18:41:57 +0300

    commands

--- 01_MainSubjects/Program.cs
+++ 01_MainSubjects/Program.cs
@@ -73,7 +73,7 @@ namespace _01_MainSubjects
 
             #endregion
 
-            #region IntegerDegiskenler
+            #region Integer Degiskenler
             //int
 
             //int number = 24;
