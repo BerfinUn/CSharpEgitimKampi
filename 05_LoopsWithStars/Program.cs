a1e72c91086e8896a1851ab5aa17fb3feecd0a6d
Berfin <101349315+BerfinUn@users.noreply.github.com>
2024-10-13 15:20:08 +0300

    creating shapes with stars

---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Alt Alta 10 Tane Yıldız Oluşturma
+
+            //for(int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            #endregion
+
+            #region Yan Yana 10 Tane Yıldız Oluşturma
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            #endregion
+
+            #region Alt Alta 10 Tane Yıldız Oluşturma ve Her Satırda 10 Tane Yıldız Olsun
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+
+            #endregion
+
+            #region Dik Üçgen
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters Dik Üçgen
+
+            //for (int i = 5; i >0; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Dik ve Ters Üçgeni Beraber Oluşturma
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int k = 4; k >= 1; k--)
+            //{
+            //    for (int m = 1; m <= k; m++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Yıldızlarla Baklava Dilimi Oluşturma
+
+            //üst kısım
+            //int n = 5;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //alt kısım
+            //for (int i = n - 1; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Piramit
+
+            //int n = 5;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    //boşluklar
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    //yıldızlar
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters Piramit
+
+            //int n = 5;
+            //for (int i = n; i >= 1; i--)
+            //{
+            //    //boşluklar 
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    //yıldızlar
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
