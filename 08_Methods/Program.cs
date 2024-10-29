d352294d9e8806742fdabb5aff2b895d053b4fe4
Berfin <101349315+BerfinUn@users.noreply.github.com>
2024-10-29 13:39:35 +0300

    📍 SQL veri tabanı, tablo oluşturma ve C# kısmına bağlantı kurma konularını tamamladım

--- 08_Methods/Program.cs
+++ 08_Methods/Program.cs
@@ -20,126 +20,126 @@ namespace _08_Methods
             //    Console.WriteLine("Hakan Öztürk");
             //    Console.WriteLine("Merve Çınar");
 
-                //}
-                //CustomerList();
+            //}
+            //CustomerList();
 
-                //void Sum()
-                //{
-                //    int x = 1;
-                //    int y = 2;
-                //    int z = x + y;
-                //    Console.WriteLine(z);
-                //}
-                //Sum();
+            //void Sum()
+            //{
+            //    int x = 1;
+            //    int y = 2;
+            //    int z = x + y;
+            //    Console.WriteLine(z);
+            //}
+            //Sum();
 
-                #endregion
+            #endregion
 
-                #region Geriye Değer Döndürmeyen String Parametreli Metotlar
+            #region Geriye Değer Döndürmeyen String Parametreli Metotlar
 
-                //void WriteMethod(string customerName)
-                //{
-                //    Console.WriteLine(customerName);
-                //}
-                //WriteMethod("Mehmet Yıldırım");
+            //void WriteMethod(string customerName)
+            //{
+            //    Console.WriteLine(customerName);
+            //}
+            //WriteMethod("Mehmet Yıldırım");
 
-                //void CustomerCard(string name, string surName)
-                //{
-                //    Console.WriteLine("Müşteri: " + name + " " + surName);
-                //}
-                //CustomerCard("Mehmet","Yıldız");
-                //CustomerCard("Ayşe","Deniz");
+            //void CustomerCard(string name, string surName)
+            //{
+            //    Console.WriteLine("Müşteri: " + name + " " + surName);
+            //}
+            //CustomerCard("Mehmet","Yıldız");
+            //CustomerCard("Ayşe","Deniz");
 
 
-                #endregion
+            #endregion
 
-                #region Geriye Değer Döndürmeyen Int Parametreli Metotlar
+            #region Geriye Değer Döndürmeyen Int Parametreli Metotlar
 
-                //void Sum(int number1, int number2, int number3)
-                //{
-                //    int result = number1 + number2 + number3;
-                //    Console.WriteLine(result);
-                //}
-                //Sum(4, 5, 6);
+            //void Sum(int number1, int number2, int number3)
+            //{
+            //    int result = number1 + number2 + number3;
+            //    Console.WriteLine(result);
+            //}
+            //Sum(4, 5, 6);
 
-                #endregion
+            #endregion
 
-                #region Geriye Değer Döndüren Metotlar
+            #region Geriye Değer Döndüren Metotlar
 
-                //string CustomerName()
-                //{
-                //    return "Buse Yıldız";
-                //}
-                //CustomerName();
+            //string CustomerName()
+            //{
+            //    return "Buse Yıldız";
+            //}
+            //CustomerName();
 
-                //string StudentCard()
-                //{
-                //    string name = "Ali";
-                //    string surname = "Yıldız";
-                //    return name + " " + surname;
-                //}
-                //Console.WriteLine(StudentCard());
+            //string StudentCard()
+            //{
+            //    string name = "Ali";
+            //    string surname = "Yıldız";
+            //    return name + " " + surname;
+            //}
+            //Console.WriteLine(StudentCard());
 
-                #endregion
+            #endregion
 
-                #region Geriye Değer Döndüren String Parametreli Metotlar
+            #region Geriye Değer Döndüren String Parametreli Metotlar
 
-                //string CountryCard(string countryName, string capital, string flagColor)
-                //{
-                //    string cardInfo = "Ülke: " + countryName + "--> Başkent:  " + capital +
-                //        "--> Bayrak Rengi:  " + flagColor;
-                //    return cardInfo;
-                //}
-                //string x, y, z;
-                //Console.Write("Ülke Adını Giriniz: ");
-                //x = Console.ReadLine();
+            //string CountryCard(string countryName, string capital, string flagColor)
+            //{
+            //    string cardInfo = "Ülke: " + countryName + "--> Başkent:  " + capital +
+            //        "--> Bayrak Rengi:  " + flagColor;
+            //    return cardInfo;
+            //}
+            //string x, y, z;
+            //Console.Write("Ülke Adını Giriniz: ");
+            //x = Console.ReadLine();
 
-                //Console.Write("Başkenti Giriniz: ");
-                //y = Console.ReadLine();
+            //Console.Write("Başkenti Giriniz: ");
+            //y = Console.ReadLine();
 
-                //Console.Write("Bayrak Rengini Giriniz: ");
-                //z = Console.ReadLine();
+            //Console.Write("Bayrak Rengini Giriniz: ");
+            //z = Console.ReadLine();
 
-                //Console.WriteLine(CountryCard(x, y, z));
+            //Console.WriteLine(CountryCard(x, y, z));
 
-                //Console.WriteLine(CountryCard("Türkiye", "Ankara", "Kırmızı-Beyaz"));
+            //Console.WriteLine(CountryCard("Türkiye", "Ankara", "Kırmızı-Beyaz"));
 
-                #endregion
+            #endregion
 
-                #region  Geriye Değer Döndüren Int Parametreli Metotlar
+            #region  Geriye Değer Döndüren Int Parametreli Metotlar
 
-                //int Sum(int number1, int number2)
-                //{
-                //    int result = number1 + number2;
-                //    return result;
-                //}
-                //Console.WriteLine(Sum(45, 98));
-                //Console.WriteLine(Sum(36, 25));
-                //Console.WriteLine(Sum(44, 36));
-                //Console.WriteLine(Sum(14, 20));
-                //Console.WriteLine(Sum(56, 65));
+            //int Sum(int number1, int number2)
+            //{
+            //    int result = number1 + number2;
+            //    return result;
+            //}
+            //Console.WriteLine(Sum(45, 98));
+            //Console.WriteLine(Sum(36, 25));
+            //Console.WriteLine(Sum(44, 36));
+            //Console.WriteLine(Sum(14, 20));
+            //Console.WriteLine(Sum(56, 65));
 
-                #endregion
+            #endregion
 
-                #region Örnek Uygulama
+            #region Örnek Uygulama
 
-                //string ExamResult(string student, int exam1, int exam2, int exam3)
-                //{
-                //    int result = (exam1 + exam2 + exam3) / 3;
-                //    if (result >= 50)
-                //    {
-                //        return student + " İsimli Öğrenci Sınavı Geçti " + "Ortalama: " + result;
-                //    }
-                //    else
-                //    {
-                //        return student + " İsimli Öğrenci Sınavı Geçemedi " + "Ortalama: " + result; ;
-                //    }
-                //}
-                //Console.WriteLine(ExamResult("Ali", 25, 41, 55));
-                //Console.WriteLine(ExamResult("Ayşe", 36, 88, 33));
+            //string ExamResult(string student, int exam1, int exam2, int exam3)
+            //{
+            //    int result = (exam1 + exam2 + exam3) / 3;
+            //    if (result >= 50)
+            //    {
+            //        return student + " İsimli Öğrenci Sınavı Geçti " + "Ortalama: " + result;
+            //    }
+            //    else
+            //    {
+            //        return student + " İsimli Öğrenci Sınavı Geçemedi " + "Ortalama: " + result; ;
+            //    }
+            //}
+            //Console.WriteLine(ExamResult("Ali", 25, 41, 55));
+            //Console.WriteLine(ExamResult("Ayşe", 36, 88, 33));
 
-                #endregion
+            #endregion
 
-                Console.Read();
+            Console.Read();
         }
     }
 }
---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ado.net SQL yapılarını kullanmamızı sağlayan kütüphane
+
+            Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi ******");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
+            tableNumber = Console.ReadLine();
+            Console.WriteLine("--------------------------------------------");
+
+            SqlConnection connection = new SqlConnection("Data Source= LAPTOP-P0ITN1L5\\SQLEXPRESS;initial Catalog = EgitimKampiDb; integrated security = true ");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select * From TblCategory", connection);
+            //c# tarafından oluşturmuş olduğumuz sorgunun sql ile arasında bir köprü görevi görmesini sağlar
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            //verilerin geçici belleğe almasını sağlar
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            connection.Close();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
